fe8870060b6d6487d2e65c6ae385a50ae211dd65
miguel <bosh99@github.com>
2022-08-19 18:50:37 -0500

    Entrega 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from re import A
 import pandas as pn
 
-#First delivery-----------------------------------------------------------------------------------#
 medellin = pn.read_csv('MedellinSH.csv', sep=';')
 medellin.harassmentRisk = medellin.harassmentRisk.fillna(medellin.harassmentRisk.mean())
-print(medellin.harassmentRisk)
-#-------------------------------------------------------------------------------------------------#                             
+
+
