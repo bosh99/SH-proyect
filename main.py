8f5ba4e40c0ef7c87992ca2e3a30f344f9160d1b
miguel <bosh99@github.com>
2022-09-28 10:51:24 -0500

    Grafo creado, falta algoritmo

--- main.py
+++ main.py
@@ -9,56 +9,6 @@ o_unicos = medellin.origin.unique()
 graph = {}
 i = 0
 
-'''
-for i in range(3):
-    print(O_unicos[i])
-    print(medellin[medellin.origin == O_unicos[i]]["harassmentRisk"])
-
-g_nodes = {}
-for i in range(2):
-    if O_unicos[i] in g_nodes:
-        g_nodes[O_unicos[i]].append(medellin[medellin.origin == O_unicos[i]]["destination"])
-    else:
-        g_nodes[O_unicos[i]] = medellin[medellin.origin == O_unicos[i]]["destination"] 
-
-print(g_nodes)
-
-graph = {}
-H_risk = medellin.harassmentRisk
-length = medellin.length 
-way = medellin.oneway
-for i in range(2):
-    graph[H_risk[i],length[i],way[i]] = g_nodes[O_unicos[i]]
-
-
-graph_nodes = g_nodes.items()
-for item in graph_nodes:
-    print(item)
-
-graph_nodes = graph.items()
-for item in graph_nodes:
-    print(item)
-    print(" ")
-
-print(o_unicos)
-for origin in o_unicos:
-    graph[origin] = []
-
-
-for row in medellin.itertuples():
-    s_harassament = row[6]
-    length = row[4]
-    if i < len(o_unicos):
-        origen = graph[o_unicos[i]]
-        i+=1
-
-    destination = {row[3] : (s_harassament, length) }
-    origen.update(destination)
-
-
-'''
-
-
 for origin in o_unicos:
     graph[origin] = []
 
