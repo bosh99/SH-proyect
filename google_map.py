421ffb55979ab4532a817542fb31af6ecc61aaa7
miguel <bosh99@github.com>
2022-10-17 12:04:02 -0500

    Entrega 2

--- google_map.py
+++ google_map.py
@@ -2,7 +2,9 @@ import gmplot
 from main import *
 
 
-
+'''
+    Open the map with the Live Server Vscode extension, right click on the Html file after running 'main.py' and 'google_map.py'
+'''
 apikey = 'AIzaSyBEkzz6baWpUTM_DhdTcK9oTxVL0r-ZXJc' # (your API key here)
 gmap = gmplot.GoogleMapPlotter(6.20020215,-75.5784848084993, 15, apikey=apikey)
 gmap.draw("map.html")
